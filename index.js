c48fcb201ca95505f5a64ab21322301f81ba8a9a
Chirag Soni <chiragsoni81245@gmail.com>
2022-04-10 21:54:41 +0530

    [Initial Commit] added basic data serving from json file

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(
+	cors({
+		origin: ["http://localhost:9000", "http://localhost:3000"],
+	})
+);
+
+app.get("/api/products", (req, res) => {
+	const products = JSON.parse(fs.readFileSync("data.json"));
+	res.send(products);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on localhost:${PORT}...`);
+});
